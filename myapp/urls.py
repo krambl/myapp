3e4172de5d8928f2cf9fc1376ff02d73b5d9c42b
U.Niakrepkin <vlniakrepkin@gmail.com>
2019-04-21 10:07:19 +0300

    Добавлена главная страница

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('webpay/', views.webpay_list, name='webpay_list'),
     path('', views.post_list, name='post_list'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def post_list(request):
     return render(request, 'myapp/main.html', {})
+
+def webpay_list(request):
+    return render(request, 'myapp/webpay.html', {})
+
