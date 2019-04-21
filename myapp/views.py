aba96dd66d92008eb297d4498754ddfe27f3fcbe
U.Niakrepkin <vlniakrepkin@gmail.com>
2019-04-21 13:38:28 +0300

    add menu

--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
 
 def post_list(request):
-    return render(request, 'myapp/main.html', {})
+    posts = Post.objects.all
+
+    return render(request, 'myapp/main.html', {'posts': posts})
 
 def webpay_list(request):
-    return render(request, 'myapp/webpay.html', {})
+    post = Post.objects.get(title="Webpay")
+    return render(request, 'myapp/webpay.html', {post})
 
